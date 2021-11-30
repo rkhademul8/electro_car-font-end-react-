6f8d873551f754a73c41373c7434645866b6590f
rkhademul8 <rkhademul8@gmail.com>
2021-11-30 16:59:11 +0600

    upto_top_rated_car

--- src/Component/Body_part/Body_part.js
+++ src/Component/Body_part/Body_part.js
@@ -14,23 +14,25 @@ const Body_part = () => {
 
              <Row>
                 <Col className="col-12 p-3"  md={3}>
-                <Card style={{ width: '18rem' }}>
+                <Card  className='images' style={{ width: '18rem' }}>
+                    
                     <Card.Img variant="top" src="images/top_rated/1.jpg" />
+                    
+                   
                     <Card.Body>
                         <Card.Title>Toyota 4Runner</Card.Title>
                         <div class="d-flex justify-content-between">
                             <p>$12000</p>
                             <div class="d-flex justify-content-between">
-                            <span> <FaStar/></span>
-                            <span> <FaStar/></span>
-                            <span> <FaStar/></span>
-                            <span> <FaStar/></span>
-                            <span> <FaStar/></span>
-                            <span>(5 Reviews)</span>
-                           
-                            
+                            <span className='pt-1'> <FaStar/></span>
+                            <span className='pt-1'> <FaStar/></span>
+                            <span className='pt-1'> <FaStar/></span>
+                            <span className='pt-1'> <FaStar/></span>
+                            <span className='pt-1'> <FaStar/></span>
+                            <p>(5 Reviews)</p>
                             </div>
                         </div>
+
                         <Card.Text className='pt-2 pb-2'>
                         Some quick example text to build on the card title and make up the bulk of
                         the card's content.
@@ -39,97 +41,202 @@ const Body_part = () => {
                     </Card.Body>
                 </Card>
                 </Col>
-                <Col className="col-12 p-3" md={3}>
-                <Card style={{ width: '18rem' }}>
-                    <Card.Img variant="top" src="images/top_rated/2.jpg"  />
+                <Col className="col-12 p-3"  md={3}>
+                <Card  className='images' style={{ width: '18rem' }}>
+                    
+                    <Card.Img variant="top" src="images/top_rated/2.jpg" />
+                    
+                   
                     <Card.Body>
-                        <Card.Title>Card Title</Card.Title>
-                        <Card.Text>
+                        <Card.Title>Toyota 4Runner</Card.Title>
+                        <div class="d-flex justify-content-between">
+                            <p>$12000</p>
+                            <div class="d-flex justify-content-between">
+                            <span className='pt-1'> <FaStar/></span>
+                            <span className='pt-1'> <FaStar/></span>
+                            <span className='pt-1'> <FaStar/></span>
+                            <span className='pt-1'> <FaStar/></span>
+                            <span className='pt-1'> <FaStar/></span>
+                            <p>(5 Reviews)</p>
+                            </div>
+                        </div>
+
+                        <Card.Text className='pt-2 pb-2'>
                         Some quick example text to build on the card title and make up the bulk of
                         the card's content.
                         </Card.Text>
-                        <Button variant="primary">Go somewhere</Button>
+                        <Button variant="primary">Buy Now</Button>
                     </Card.Body>
                 </Card>
                 </Col>
                 <Col className="col-12 p-3"  md={3}>
-                <Card style={{ width: '18rem' }}>
-                    <Card.Img variant="top" src="images/top_rated/3.jpg"  />
+                <Card  className='images' style={{ width: '18rem' }}>
+                    
+                    <Card.Img variant="top" src="images/top_rated/3.jpg" />
+                    
+                   
                     <Card.Body>
-                        <Card.Title>Card Title</Card.Title>
-                        <Card.Text>
+                        <Card.Title>Toyota 4Runner</Card.Title>
+                        <div class="d-flex justify-content-between">
+                            <p>$12000</p>
+                            <div class="d-flex justify-content-between">
+                            <span className='pt-1'> <FaStar/></span>
+                            <span className='pt-1'> <FaStar/></span>
+                            <span className='pt-1'> <FaStar/></span>
+                            <span className='pt-1'> <FaStar/></span>
+                            <span className='pt-1'> <FaStar/></span>
+                            <p>(5 Reviews)</p>
+                            </div>
+                        </div>
+
+                        <Card.Text className='pt-2 pb-2'>
                         Some quick example text to build on the card title and make up the bulk of
                         the card's content.
                         </Card.Text>
-                        <Button variant="primary">Go somewhere</Button>
+                        <Button variant="primary">Buy Now</Button>
                     </Card.Body>
                 </Card>
                 </Col>
                 <Col className="col-12 p-3"  md={3}>
-                <Card style={{ width: '18rem' }}>
-                    <Card.Img variant="top" src="images/top_rated/4.jpg"  />
+                <Card  className='images' style={{ width: '18rem' }}>
+                    
+                    <Card.Img variant="top" src="images/top_rated/4.jpg" />
+                    
+                   
                     <Card.Body>
-                        <Card.Title>Card Title</Card.Title>
-                        <Card.Text>
+                        <Card.Title>Toyota 4Runner</Card.Title>
+                        <div class="d-flex justify-content-between">
+                            <p>$12000</p>
+                            <div class="d-flex justify-content-between">
+                            <span className='pt-1'> <FaStar/></span>
+                            <span className='pt-1'> <FaStar/></span>
+                            <span className='pt-1'> <FaStar/></span>
+                            <span className='pt-1'> <FaStar/></span>
+                            <span className='pt-1'> <FaStar/></span>
+                            <p>(5 Reviews)</p>
+                            </div>
+                        </div>
+
+                        <Card.Text className='pt-2 pb-2'>
                         Some quick example text to build on the card title and make up the bulk of
                         the card's content.
                         </Card.Text>
-                        <Button variant="primary">Go somewhere</Button>
+                        <Button variant="primary">Buy Now</Button>
                     </Card.Body>
                 </Card>
                 </Col>
             </Row>
 
             <Row>
-                <Col className="col-12 p-3"  md={3}>
-                <Card style={{ width: '18rem' }}>
+            <Col className="col-12 p-3"  md={3}>
+                <Card  className='images' style={{ width: '18rem' }}>
+                    
                     <Card.Img variant="top" src="images/top_rated/5.jpg" />
+                    
+                   
                     <Card.Body>
-                        <Card.Title>Card Title</Card.Title>
-                        <Card.Text>
+                        <Card.Title>Toyota 4Runner</Card.Title>
+                        <div class="d-flex justify-content-between">
+                            <p>$12000</p>
+                            <div class="d-flex justify-content-between">
+                            <span className='pt-1'> <FaStar/></span>
+                            <span className='pt-1'> <FaStar/></span>
+                            <span className='pt-1'> <FaStar/></span>
+                            <span className='pt-1'> <FaStar/></span>
+                            <span className='pt-1'> <FaStar/></span>
+                            <p>(5 Reviews)</p>
+                            </div>
+                        </div>
+
+                        <Card.Text className='pt-2 pb-2'>
                         Some quick example text to build on the card title and make up the bulk of
                         the card's content.
                         </Card.Text>
-                        <Button variant="primary">Go somewhere</Button>
-                    </Card.Body>
-                </Card>
-                </Col>
-                <Col className="col-12 p-3" md={3}>
-                <Card style={{ width: '18rem' }}>
-                    <Card.Img variant="top" src="images/top_rated/6.jpg"  />
-                    <Card.Body>
-                        <Card.Title>Card Title</Card.Title>
-                        <Card.Text>
-                        Some quick example text to build on the card title and make up the bulk of
-                        the card's content.
-                        </Card.Text>
-                        <Button variant="primary">Go somewhere</Button>
-                    </Card.Body>
-                </Card>
-                </Col>
-                <Col className="col-12 p-3"  md={3}>
-                <Card style={{ width: '18rem' }}>
-                    <Card.Img variant="top" src="images/a.jpg"  />
-                    <Card.Body>
-                        <Card.Title>Card Title</Card.Title>
-                        <Card.Text>
-                        Some quick example text to build on the card title and make up the bulk of
-                        the card's content.
-                        </Card.Text>
-                        <Button variant="primary">Go somewhere</Button>
+                        <Button variant="primary">Buy Now</Button>
                     </Card.Body>
                 </Card>
                 </Col>
                 <Col className="col-12 p-3"  md={3}>
-                <Card style={{ width: '18rem' }}>
-                    <Card.Img variant="top" src="images/b.jpg"  />
+                <Card  className='images' style={{ width: '18rem' }}>
+                    
+                    <Card.Img variant="top" src="images/top_rated/6.jpg" />
+                    
+                   
                     <Card.Body>
-                        <Card.Title>Card Title</Card.Title>
-                        <Card.Text>
+                        <Card.Title>Toyota 4Runner</Card.Title>
+                        <div class="d-flex justify-content-between">
+                            <p>$12000</p>
+                            <div class="d-flex justify-content-between">
+                            <span className='pt-1'> <FaStar/></span>
+                            <span className='pt-1'> <FaStar/></span>
+                            <span className='pt-1'> <FaStar/></span>
+                            <span className='pt-1'> <FaStar/></span>
+                            <span className='pt-1'> <FaStar/></span>
+                            <p>(5 Reviews)</p>
+                            </div>
+                        </div>
+
+                        <Card.Text className='pt-2 pb-2'>
                         Some quick example text to build on the card title and make up the bulk of
                         the card's content.
                         </Card.Text>
-                        <Button variant="primary">Go somewhere</Button>
+                        <Button variant="primary">Buy Now</Button>
+                    </Card.Body>
+                </Card>
+                </Col>
+                <Col className="col-12 p-3"  md={3}>
+                <Card  className='images' style={{ width: '18rem' }}>
+                    
+                    <Card.Img variant="top" src="images/a.jpg" />
+                    
+                   
+                    <Card.Body>
+                        <Card.Title>Toyota 4Runner</Card.Title>
+                        <div class="d-flex justify-content-between">
+                            <p>$12000</p>
+                            <div class="d-flex justify-content-between">
+                            <span className='pt-1'> <FaStar/></span>
+                            <span className='pt-1'> <FaStar/></span>
+                            <span className='pt-1'> <FaStar/></span>
+                            <span className='pt-1'> <FaStar/></span>
+                            <span className='pt-1'> <FaStar/></span>
+                            <p>(5 Reviews)</p>
+                            </div>
+                        </div>
+
+                        <Card.Text className='pt-2 pb-2'>
+                        Some quick example text to build on the card title and make up the bulk of
+                        the card's content. 
+                        </Card.Text>
+                        <Button variant="primary">Buy Now</Button>
+                    </Card.Body>
+                </Card>
+                </Col>
+                <Col className="col-12 p-3"  md={3}>
+                <Card  className='images' style={{ width: '18rem' }}>
+                    
+                    <Card.Img variant="top" src="images/b.jpg" />
+                    
+                   
+                    <Card.Body>
+                        <Card.Title>Toyota 4Runner</Card.Title>
+                        <div class="d-flex justify-content-between">
+                            <p>$12000</p>
+                            <div class="d-flex justify-content-between">
+                            <span className='pt-1'> <FaStar/></span>
+                            <span className='pt-1'> <FaStar/></span>
+                            <span className='pt-1'> <FaStar/></span>
+                            <span className='pt-1'> <FaStar/></span>
+                            <span className='pt-1'> <FaStar/></span>
+                            <p>(5 Reviews)</p>
+                            </div>
+                        </div>
+
+                        <Card.Text className='pt-2 pb-2'>
+                        Some quick example text to build on the card title and make up the bulk of
+                        the card's content.
+                        </Card.Text>
+                        <Button variant="primary">Buy Now</Button>
                     </Card.Body>
                 </Card>
                 </Col>
@@ -137,6 +244,54 @@ const Body_part = () => {
             
         </Container>
   
+{/* Top rated car part end*/}
+
+        <Container className='cust_width margin_top'>
+            <Row>
+                <Col className="col-12 col-md-6 p-3">
+                    <img src="images/speakers.png"></img>
+                </Col>
+                <Col  className="col-12 col-md-6 p-3">
+                <img src="images/schedule.png"></img>
+                </Col>
+            </Row>
+        </Container>
+
+{/* Best Seller Car part start */}
+        <Container className='cust_width margin_top'>
+        <h1>Best Seller Car</h1>
+        <Row>
+        <Col className="col-12 p-3"  md={3}>
+                <Card  className='images' style={{ width: '18rem' }}>
+                    
+                    <Card.Img variant="top" src="images/top_rated/6.jpg" />
+                    
+                   
+                    <Card.Body>
+                        <Card.Title className='text-center  pt-3 pb-1'>Mercedes-Benz M-class</Card.Title>
+                       <hr className='middle_border'></hr>
+                       <h3 className='p-2 text-center'>$ 95,000</h3>
+                        <div class="d-flex justify-content-between p-4">
+                            <div>
+                                <span className='pt-1'><FaStar/></span>
+                                <h4>8500</h4>
+                             </div>
+                            <div >
+                                <span className='pt-1'> <FaStar/></span>
+                                <h4>Automatic</h4>
+                            </div>
+                            <div>
+                                <span className='pt-1'> <FaStar/></span>
+                                <h4>2015</h4>
+                            </div>
+                        </div>
+
+                       
+                    </Card.Body>
+                </Card>
+                </Col>
+                </Row>
+        </Container>
 
         </div>
     );
